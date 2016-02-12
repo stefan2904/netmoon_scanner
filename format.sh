29f1f8b8074daecd0e5db4645499f133f887f687
stefan <dev@2904.cc>
2016-02-12 03:58:32 +0100

    updated docs of format script

--- format.sh
+++ format.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# formats all python files according to PEP 8.
+
+# this script can be used as commit-hook,
+# simply move it to .git/hooks/pre-commit
+
 autopep8 -r --in-place --aggressive --aggressive *.py
