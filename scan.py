3e672908c374c1c0b75b603ef5d86bdedc08b929
stefan <dev@2904.cc>
2016-03-12 22:37:33 +0100

    Implemented arping timeout, catched exception, added debug output

--- scan.py
+++ scan.py
@@ -7,6 +7,7 @@ import socket
 import nmap
 import math
 import click
+import traceback
 import os
 
 
@@ -17,6 +18,9 @@ if os.geteuid() != 0:
 def doPortscan(target_ip, portsToScan):
     nm = nmap.PortScanner()
     nm.scan(target_ip, portsToScan)
+    if target_ip not in nm.all_hosts():
+        print '      error while scanning host.'
+        return
     scan = nm[target_ip]
 
     for protocol in scan.all_protocols():
@@ -31,6 +35,8 @@ def doPortscan(target_ip, portsToScan):
                                                    info['product'],
                                                    info['version'],
                                                    info['extrainfo'])
+    if len(scan.all_protocols()) <= 0:
+        print '      no open ports found.'
 
 
 def ddn2cidr(bytes_network, bytes_netmask):
@@ -58,11 +64,12 @@ def getActiveRoutes():
 
 
 def getHostsInNet(net, interface):
+    ans = []
     try:
         ans, unans = scapy.layers.l2.arping(net, iface=interface,
-                                            timeout=1, verbose=False)
+                                            timeout=10, verbose=False)
     except:
-        pass
+        traceback.print_exc()
     return ans
 
 
@@ -76,12 +83,22 @@ def getHostsInNetwork(network, netmask, interface):
     '--ports',
     default='22-443',
     help='Portrange to scan. \n(Format: https://nmap.org/book/man-port-specification.html)')
-def main(ports):
+@click.option(
+    '--arptimeout',
+    default='10',
+    help='ARPing timeout')
+def main(ports, arptimeout):
     """
     Simple Network-Scanner.
 
     Try do discover all hosts in the (local) network & scan their ports.
     """
+
+    print '############################################'
+    print 'Welcome to netmoon scanner!'
+    print 'Scanning ports:  %s' % ports
+    print 'ARPing timemout: %s' % arptimeout
+    print '############################################'
 
     routes = getActiveRoutes()
 
